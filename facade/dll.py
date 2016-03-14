75e909921568b165582f470fc6159cbc9857855b
Alon Niv <oakfang@gmail.com>
2016-03-14 11:12:58 +0200

    Add possibility to use headers

--- facade/dll.py
+++ facade/dll.py
@@ -12,10 +12,12 @@ from .base import Finder, Loader
 
 
 class DllModule(ModuleType):
-    def __init__(self, name, ffi, lib):
+    def __init__(self, name, ffi, lib, headers):
         super(DllModule, self).__init__(name)
         self._lib = lib
         self._ffi = ffi
+        for header in headers:
+            self.declare(header)
 
     def declare(self, signature):
         self._ffi.cdef(signature)
@@ -29,21 +31,26 @@ class DllFinder(Finder):
         pass
 
     def find_module(self, name, m_path):
-        name += '.dll'
         if m_path is not None:
             name = path.join(m_path[0], name)
-        if path.exists(name):
-            return DllLoader(name)
+        source = name + '.dll'
+        headers = name + '.h'
+        if path.exists(source):
+            return DllLoader(source, headers if path.exists(headers) else None)
 
 
 class DllLoader(Loader):
-    def __init__(self, file_path):
+    def __init__(self, file_path, headers_path):
         ffi = FFI()
         self.ffi = ffi
         self.lib = ffi.dlopen(file_path)
+        self.headers = []
+        if headers_path is not None:
+            with open(headers_path) as headers:
+                self.headers = headers.readlines()
 
     def load_module(self, name):
-        return DllModule(name, self.ffi, self.lib)
+        return DllModule(name, self.ffi, self.lib, self.headers)
 
 
 DllFinder().register()
